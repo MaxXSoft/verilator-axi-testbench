96f9c934a04190518c982ffc625dd12055846815
MaxXSoft <x@MaxXSoft.net>
2026-07-15 22:58:23 +0800

    Fix clang-format issues

--- examples/riscv-common/software/include/riscv_test.h
+++ examples/riscv-common/software/include/riscv_test.h
@@ -10,11 +10,11 @@
 #define RVTEST_RV32U
 #define RVTEST_RV32M
 
-#define RVTEST_CODE_BEGIN         \
-  .section.text, "ax", @progbits; \
-  .align 2;                       \
-  .globl axi_tb_test_entry;       \
-  axi_tb_test_entry:
+#define RVTEST_CODE_BEGIN          \
+  .section .text, "ax", @progbits; \
+  .align 2;                        \
+  .globl axi_tb_test_entry;        \
+axi_tb_test_entry:
 
 #define RVTEST_CODE_END unimp
 
@@ -23,25 +23,28 @@
   fence;                   \
   li t6, AXI_TB_EXIT_BASE; \
   sw zero, 0(t6);          \
-  .Laxi_tb_pass_halt : j.Laxi_tb_pass_halt
+.Laxi_tb_pass_halt:        \
+  j .Laxi_tb_pass_halt
 
 /* Preserve the failing test number in gp; defensively make zero nonzero. */
-#define RVTEST_FAIL                                \
-  fence;                                           \
-  bnez TESTNUM, .Laxi_tb_fail_nonzero;             \
-  li TESTNUM, 1;                                   \
-  .Laxi_tb_fail_nonzero : li t6, AXI_TB_EXIT_BASE; \
-  sw TESTNUM, 0(t6);                               \
-  .Laxi_tb_fail_halt : j.Laxi_tb_fail_halt
+#define RVTEST_FAIL                    \
+  fence;                               \
+  bnez TESTNUM, .Laxi_tb_fail_nonzero; \
+  li TESTNUM, 1;                       \
+.Laxi_tb_fail_nonzero:                 \
+  li t6, AXI_TB_EXIT_BASE;             \
+  sw TESTNUM, 0(t6);                   \
+.Laxi_tb_fail_halt:                    \
+  j .Laxi_tb_fail_halt
 
 #define RVTEST_DATA_BEGIN \
   .align 4;               \
   .globl begin_signature; \
-  begin_signature:
+begin_signature:
 
 #define RVTEST_DATA_END \
   .align 4;             \
   .globl end_signature; \
-  end_signature:
+end_signature:
 
 #endif /* AXI_TB_RISCV_TEST_H */
